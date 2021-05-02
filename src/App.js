ae886cdcb40dc2cf85b868c50ee1a88852c538e8
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-05-03 00:32:41 +0300

    new size context and design fixes

--- src/App.js
+++ src/App.js
@@ -6,17 +6,22 @@ import { SignInUp } from "./pages/sign-in-up/SignInUp";
 import React from "react";
 import MainPage from "./pages/main-page/MainPage";
 import { MenuProvider } from "./contexts/nav-menu-context/NavMenuContext";
+import { SizeProvider } from "./contexts/mobile-sizes-context/MobileSizesContext";
 
 export default function App() {
   return (
     <div className="App">
       <Switch>
         <Route path="/giris">
-          <SignInUp />
+          <SizeProvider>
+            <SignInUp />
+          </SizeProvider>
         </Route>
         <Route path="/">
           <MenuProvider>
-            <MainPage />
+            <SizeProvider>
+              <MainPage />
+            </SizeProvider>
           </MenuProvider>
         </Route>
       </Switch>
