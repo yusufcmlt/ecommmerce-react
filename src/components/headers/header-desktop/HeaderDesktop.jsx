c781a102a7b850d62b6c2ebe4350ebca3c8cf0bc
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-05-03 00:33:32 +0300

    html change

--- src/components/headers/header-desktop/HeaderDesktop.jsx
+++ src/components/headers/header-desktop/HeaderDesktop.jsx
@@ -1,8 +1,5 @@
-import react from "react";
 import React from "react";
-import { Link } from "react-router-dom";
-import { auth } from "../../../firebase/firebase";
-import CustomButton from "../../buttons/custom-button/CustomButton";
+
 import { LogoDesktop } from "../../logos/logo-desktop/LogoDesktop";
 import MenuNav from "../../menu-nav/MenuNav";
 import UserGreet from "../../user-greet-message/UserGreet";
@@ -11,12 +8,12 @@ import "./HeaderDesktop.style.scss";
 
 export default function HeaderDesktop() {
   return (
-    <div className="header-desktop-container box-shadow-rule">
+    <header className="header-desktop-container box-shadow-rule">
       <LogoDesktop />
       <UserGreet />
       <div className="header-desktop-buttons">
         <MenuNav navType="header-menu" />
       </div>
-    </div>
+    </header>
   );
 }
