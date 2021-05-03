b8eabcd6083bbf5b1a20be8b9fcea5100e87fdef
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-05-03 14:33:43 +0300

    added footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Footer.style.scss";
+export default function Footer() {
+  return (
+    <div className="footer-container">
+      <li>İletişim</li>
+      <li>Kampanyalar</li>
+      <li>Sıkça Sorulan Sorular</li>
+      <li>İade ve Değişim</li>
+    </div>
+  );
+}
