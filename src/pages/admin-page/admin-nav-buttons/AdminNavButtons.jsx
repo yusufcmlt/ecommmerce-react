6af7492ea3139a01dd645dc74f94dff292b1ac27
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-05-10 01:07:36 +0300

    admin dashboard props change

--- src/pages/admin-page/admin-nav-buttons/AdminNavButtons.jsx
+++ src/pages/admin-page/admin-nav-buttons/AdminNavButtons.jsx
@@ -1,33 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+
 import CustomButton from "../../../components/buttons/custom-button/CustomButton";
+import Loading from "../../../components/loading/Loading";
+import { useItems } from "../../../contexts/item-category-context/ItemCategoryContext";
 
 import "../AdminPage.style.scss";
 
-export default function AdminNavButtons({
-  productCount,
-  categoryCount,
-  navPosition,
-  handlePageTitle,
-}) {
+export default function AdminNavButtons({ navPosition }) {
+  const { handleCountLoading, counts } = useItems();
+
+  useEffect(() => {
+    if (!counts.loaded) {
+      handleCountLoading();
+    }
+  }, []);
+
   return (
     <nav className={`admin-nav-buttons admin-nav-${navPosition}`}>
-      <Link to="/yonetim/urunler">
-        <CustomButton
-          buttonText={productCount}
-          buttonSize="admin-nav"
-          buttonIcon="product"
-          buttonInfo="Ürünler"
-        />
-      </Link>
-      <Link to="/yonetim/kategoriler">
-        <CustomButton
-          buttonText={categoryCount}
-          buttonSize="admin-nav"
-          buttonIcon="category"
-          buttonInfo="Kategoriler"
-        />
-      </Link>
+      {counts.loaded ? (
+        <React.Fragment>
+          <Link to="/yonetim/urunler" className="admin-nav-link">
+            <CustomButton
+              buttonText={counts.items}
+              buttonSize="admin-nav"
+              buttonIcon="product"
+              buttonInfo="Ürünler"
+            />
+          </Link>
+          <Link to="/yonetim/kategoriler" className="admin-nav-link">
+            <CustomButton
+              buttonText={counts.categories}
+              buttonSize="admin-nav"
+              buttonIcon="category"
+              buttonInfo="Kategoriler"
+            />
+          </Link>
+        </React.Fragment>
+      ) : (
+        <Loading size="page" />
+      )}
     </nav>
   );
 }
