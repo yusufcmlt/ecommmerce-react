e93b563464ba8350090587ed25f6119187b3f59f
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-05-10 20:09:33 +0300

    removed category form

--- src/pages/admin-page/admin-forms/AdminCategoryForm.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function AdminCategoryMenu() {
-  return <div>CategoryMenu</div>;
-}
-
-export function AdminCategoryForm() {
-  return <div>CategoryForm</div>;
-}
