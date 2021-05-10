91e471b21c8bf708fc7fd22b5fa07faf44fb2c45
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-05-10 20:08:17 +0300

    new select component

--- src/pages/admin-page/admin-forms/AdminProductForm.jsx
+++ src/pages/admin-page/admin-forms/AdminProductForm.jsx
@@ -2,19 +2,27 @@ import React from "react";
 import CustomButton from "../../../components/buttons/custom-button/CustomButton";
 import CustomSelect from "../../../components/buttons/custom-select/CustomSelect";
 import CustomInput from "../../../components/custom-input/CustomInput";
+import { useItems } from "../../../contexts/item-category-context/ItemCategoryContext";
 
-export function AdminProductMenu() {
+export function AdminFilterMenu({ filter, filterOption, input }) {
+  const { handleFiltering, handleSorting } = useItems();
   return (
     <>
       <CustomInput
         inputType="text"
-        inputName="adminProductSearch"
-        inputPlaceholder="Ürün Ara"
+        inputName={input}
+        inputPlaceholder={`${filter} Ara`}
         inputSize="admin"
+        inputChange={handleFiltering}
       />
-      <CustomSelect />
+      <CustomSelect
+        size="select-size-admin"
+        options={filterOption}
+        selectName={input}
+        selectOnChange={handleSorting}
+      />
       <CustomButton
-        buttonText="Ürün Ekle"
+        buttonText={`${filter} Ekle`}
         buttonSize="admin-menu"
         buttonIcon="plus"
       />
@@ -24,4 +32,8 @@ export function AdminProductMenu() {
 
 export function AdminProductForm() {
   return <div>PRODUCT FORM</div>;
+}
+
+export function AdminCategoryForm() {
+  return <div>category form</div>;
 }
