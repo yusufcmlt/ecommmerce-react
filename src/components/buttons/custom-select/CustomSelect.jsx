7a9cf7d9095434e26647c083553ad0917fae4097
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-05-10 20:05:55 +0300

    new custom select component

--- src/components/buttons/custom-select/CustomSelect.jsx
+++ src/components/buttons/custom-select/CustomSelect.jsx
@@ -1,5 +1,24 @@
 import React from "react";
 
-export default function CustomSelect() {
-  return <select></select>;
+import "./CustomSelect.style.scss";
+
+export default function CustomSelect({
+  size,
+  options,
+  selectName,
+  selectOnChange,
+}) {
+  return (
+    <select
+      name={selectName}
+      className={`custom-select-container ${size} input-stroke`}
+      onChange={selectOnChange}
+    >
+      {options.map((option) => (
+        <option key={`option-${option.value}`} value={option.value}>
+          {option.name}
+        </option>
+      ))}
+    </select>
+  );
 }
