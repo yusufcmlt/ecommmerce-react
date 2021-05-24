ea5b8f19906bd75695e3f685b34a98162345e53d
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-05-25 01:12:55 +0300

    cart item and cart item quantity design

--- src/pages/cart-page/CartItem.jsx
+++ src/pages/cart-page/CartItem.jsx
@@ -1,22 +1,35 @@
 import React from "react";
+import { useHistory } from "react-router";
+import CustomButton from "../../components/buttons/custom-button/CustomButton";
 
 import "./CartPage.style.scss";
 
-export default function CartItem({ cartData }) {
-  const { name, imageURL, price, quantity } = cartData;
+export default function CartItem({ cartData, itemDelete }) {
+  const history = useHistory();
+  const { id, name, imageURL, price, quantity } = cartData;
+
   return (
     <div className="cart-item-container">
       <img src={imageURL} className="cart-item-image" alt="item" />
-      <div className="cart-item-info">
+      <div
+        className="cart-item-info"
+        onClick={() => {
+          history.push({ pathname: `/urun/${id}` });
+        }}
+      >
         <h4>{name}</h4>
+        <span>Ürün miktarı:{quantity} Kg</span>
         <span>
           Birim fiyat:{price}
           <span className="currency-symbol">₺</span>
         </span>
       </div>
       <div className="cart-item-options">
-        <span>{quantity}</span>
-        <span>Item Delete</span>
+        <CustomButton
+          buttonSize="edit"
+          buttonIcon="delete"
+          funcOnPress={itemDelete}
+        />
       </div>
     </div>
   );
