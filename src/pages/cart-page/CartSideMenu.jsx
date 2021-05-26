5414fbcd9e5c3d7b3161cece1e0df2de25a6ffa4
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-05-26 23:40:54 +0300

    adress preview on cart order menu

--- src/pages/cart-page/CartSideMenu.jsx
+++ src/pages/cart-page/CartSideMenu.jsx
@@ -1,20 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router";
+
 import CustomButton from "../../components/buttons/custom-button/CustomButton";
 import CustomSelect from "../../components/buttons/custom-select/CustomSelect";
+import { useCart } from "../../contexts/cart-context/CartContext";
+import AddressItem from "../address-page/AddressItem";
 
 import "./CartPage.style.scss";
+import "../address-page/AddressPage.style.scss";
+import CartAddressItem from "./CartAddressItem";
 
 export default function CartSideMenu({ total }) {
   const history = useHistory();
+  const { addressData, handleAddressLoad } = useCart();
+
+  const [selectedAddress, setSelectedAddress] = useState({});
+
+  useEffect(() => {
+    if (!addressData.loaded) {
+      handleAddressLoad();
+    }
+  }, []);
+
+  function createAddressOption() {
+    if (addressData.loaded) {
+      return addressData.data.map((address) => ({
+        name: address.name,
+        value: address.id,
+      }));
+    } else return [];
+  }
+
+  function handleSelectChange(event) {
+    const { value } = event.target;
+    console.log(value);
+    if (value) {
+      setSelectedAddress(
+        addressData.data.filter((address) => address.id === value)[0]
+      );
+    } else {
+      setSelectedAddress({});
+    }
+  }
 
   return (
     <div className="cart-menu-container">
       <h3 className="cart-order-title">Adres</h3>
-      <CustomSelect options={[]} size="cart-address-select" />
+      <CustomSelect
+        options={createAddressOption()}
+        size="cart-address-select"
+        selectOnChange={handleSelectChange}
+      />
+      {Object.keys(selectedAddress).length ? (
+        <CartAddressItem data={selectedAddress} />
+      ) : null}
       <CustomButton
         buttonSize="admin-menu size-sidemenu"
-        buttonText="Adres Ekle"
+        buttonText="Yeni Adres Ekle"
         funcOnPress={() => {
           history.push({ pathname: "/adreslerim" });
         }}
