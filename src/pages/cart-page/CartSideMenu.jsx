00c12a80df5ccc8768ff8e35ed86cd5d25a535be
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-05-26 16:03:20 +0300

    add address select

--- src/pages/cart-page/CartSideMenu.jsx
+++ src/pages/cart-page/CartSideMenu.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { useHistory } from "react-router";
 import CustomButton from "../../components/buttons/custom-button/CustomButton";
+import CustomSelect from "../../components/buttons/custom-select/CustomSelect";
 
 import "./CartPage.style.scss";
 
 export default function CartSideMenu({ total }) {
+  const history = useHistory();
+
   return (
     <div className="cart-menu-container">
+      <h3 className="cart-order-title">Adres</h3>
+      <CustomSelect options={[]} size="cart-address-select" />
+      <CustomButton
+        buttonSize="admin-menu size-sidemenu"
+        buttonText="Adres Ekle"
+        funcOnPress={() => {
+          history.push({ pathname: "/adreslerim" });
+        }}
+      />
       <h3 className="cart-order-title">Sipariş Özeti</h3>
       <div className="cart-order-container">
         <span className="cart-order-info">
