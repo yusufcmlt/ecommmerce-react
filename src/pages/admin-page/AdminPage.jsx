92bae3e5b5dea1fdd06ef95d6e6bdd995bf63314
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-05-26 16:03:43 +0300

    delete unnecessary log

--- src/pages/admin-page/AdminPage.jsx
+++ src/pages/admin-page/AdminPage.jsx
@@ -25,7 +25,6 @@ export default function AdminPage() {
   useEffect(() => {
     let locationPath = location.pathname.split("/");
     locationPath = locationPath[locationPath.length - 1];
-    console.log(locationPath);
     setPageHeaderInfo({ ...routeInfo[locationPath] });
   }, [location]);
 
