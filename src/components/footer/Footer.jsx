437cc569bc2e74e24720e37b36fa054f1aab6ba2
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-05-29 18:34:38 +0300

    new github link

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,6 +4,11 @@ import "./Footer.style.scss";
 export default function Footer() {
   return (
     <div className="footer-container">
+      <li className="github-link">
+        <a href="https:/github.com/yusufcmlt" target="_blank" rel="noreferrer">
+          {""}
+        </a>
+      </li>
       <li>İletişim</li>
       <li>Kampanyalar</li>
       <li>Sıkça Sorulan Sorular</li>
